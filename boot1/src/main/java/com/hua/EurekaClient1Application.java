8720b343c6c899dc7665ff069dd80e2090e60ff9
lianghuating <lianghuating@baoxiucai.com>
2019-02-27 11:09:06 +0800

    文件

---
+++ boot1/src/main/java/com/hua/EurekaClient1Application.java
@@ -0,0 +1,13 @@
+package com.hua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClient1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClient1Application.class, args);
+    }
+}
