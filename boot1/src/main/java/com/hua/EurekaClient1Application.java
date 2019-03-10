4f33a867f3f007826c58e27eeafafdff1f1a6668
hua-1 <qq15538668842>
2019-03-10 18:27:11 +0800

    配置

--- boot1/src/main/java/com/hua/EurekaClient1Application.java
+++ boot1/src/main/java/com/hua/EurekaClient1Application.java
@@ -3,11 +3,10 @@ package com.hua;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@EnableFeignClients
+//@EnableFeignClients
 public class EurekaClient1Application {
     public static void main(String[] args) {
         SpringApplication.run(EurekaClient1Application.class, args);
